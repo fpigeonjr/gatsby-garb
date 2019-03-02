9c26ca22308794dffffba5cda0dc23fbfd73ee60
Frank Pigeon <frank.pigeonjr@gmail.com>
2019-03-01 22:42:40 -0600

    adds page 3

---
+++ src/pages/posts/page-3.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../../components/layout"
+import { Link } from "gatsby"
+
+export default () => (
+  <Layout>
+    <h1>Hello from Page 3</h1>
+    <Link to="/page-2">Go to Page 2</Link>
+  </Layout>
+)
